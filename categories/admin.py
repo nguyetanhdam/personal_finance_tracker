9c663f31838b28e1900e6d05f5480422fcc68014
Nguyet Anh <anhdn@avtvn.com>
2024-12-11 15:01:11 +0700

    add admin page categories

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Category)
+class CategoriesAdmin(admin.ModelAdmin):
+    search_fields = []
+    list_display = ['id', 'name', 'type', 'user']
+    list_display_links = ['id']
+    list_filter = ['type', 'user']
+    list_editable = []
+    ordering = []
