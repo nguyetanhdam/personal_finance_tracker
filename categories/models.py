2bab9e38506db691ded2fbbad343f185e33b8966
Nguyet Anh <anhdn@avtvn.com>
2024-12-11 14:33:42 +0700

    update

---
+++ categories/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TransactionType(models.IntegerChoices):
+    INCOME = 1, "Income"
+    EXPENSE = 2, "Expense"
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.PositiveSmallIntegerField(choices=TransactionType.choices, default=TransactionType.INCOME)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, db_index=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
+    
+    def get_type_display(self):
+        for value, label in TransactionType.choices:
+            if value == self.type:
+                return {"id": value, "name": label}
+        return 'Unknown Type'
--- personal_finance_tracker/settings.py
+++ personal_finance_tracker/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'personal_finance_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'finance_tracker',
+        'USER': 'anhdn',
+        'PASSWORD': 'xxdyhp',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
