35e6495fa8b1c06ce03dd5bd9aa5fbeed2386186
Nguyet Anh <anhdn@avtvn.com>
2024-12-11 10:59:41 +0700

    user app

--- user/models.py
+++ user/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(null=True, blank=True)
-    password = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return f"{self.id}_{self.username}" 
